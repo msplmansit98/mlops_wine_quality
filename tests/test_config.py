e35f61a04bea706e1e1237b0199aefbf1e455d74
Mansit  Suman <mansit.suman@themathcompany.com>
2022-06-07 17:22:00 +0530

    MS: Added pytest cases + the range for which input values are valid

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,8 +1,22 @@
 
 
 #For pytest to recognize always prefix a function with test_
+import pytest
+
+class NotInRange(Exception):
+    def __init__(self,message = "value not in range"):
+        #self.input  = input_
+        self.message = message
+        super().__init__(self.message)
 
 def test_generic():
     a = 2
-    b = 2
-    assert a==b 
+
+    with pytest.raises(NotInRange):
+        if a not in range(10,20):
+            raise NotInRange    
+
+
+def test_something():
+
+ 
